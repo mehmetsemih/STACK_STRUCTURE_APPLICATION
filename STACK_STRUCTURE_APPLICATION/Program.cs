e5b4138f8be4dfc27683314c6036bdbc8b195468
mehmetsemih <mehmetsemihunal@gmail.com>
2024-07-23 17:21:25 +0300

    STEPING_SOLUTION_APPLICATION USING THE_STACK_STRUCTURE

--- STACK_STRUCTURE_APPLICATION/Program.cs
+++ STACK_STRUCTURE_APPLICATION/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //STACK APPLICATION
+
+            Console.WriteLine("LÜTFEN BİR SAYI GİRİNİZ: ");
+            int num1=Convert.ToInt32(Console.ReadLine());
+
+            var sayı_yığını=new Stack<int>();
+             while (num1 > 0)
+            {
+                int k = num1 % 10;
+                sayı_yığını.Push(k);
+                num1 = num1/10;
+            }
+
+            int i = 0;
+            int n = sayı_yığını.Count-1;
+            foreach (var s in sayı_yığını)
+            {
+                Console.WriteLine($"{s} x {Math.Pow(10,n-i)} = {s* Math.Pow(10, n - i)}");
+                i++;
+            }
+
+
+
+            Console.ReadLine();
+
         }
     }
 }
