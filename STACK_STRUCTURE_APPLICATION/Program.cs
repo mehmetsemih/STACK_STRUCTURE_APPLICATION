eea7ddf9fa6b836fe32349a85b0afc88c525717b
mehmetsemih <mehmetsemihunal@gmail.com>
2024-07-23 16:56:53 +0300

    Proje dosyası ekle.

---
+++ STACK_STRUCTURE_APPLICATION/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace STACK_STRUCTURE_APPLICATION
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
